29c82df1a096087626796a4d1dd0cb8977c8a528
Hamdy Abu El-Khair <hamdyabuelkhair@gmail.com>
2026-02-05 21:06:22 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ if (typeof globalThis.structuredClone !== "function") {
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: "/hamdy-hamdy-portofolio",
+    base: "/hamdy-portofolio",
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
