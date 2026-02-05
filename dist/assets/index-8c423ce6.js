35ad2fac02a1bc2fd74806de243c1a930b90975e
7amdyabuelkhair <hamdyabuelkhair@gmail.com>
2026-02-05 20:57:17 +0200

    add dist folder for github pages

--- dist/assets/index-8c423ce6.js
+++ dist/assets/index-8c423ce6.js
@@ -1,4 +1,4 @@
-import{g as er,R as Gn,c as Se,r as ae,a as tr}from"./index-d94d7634.js";/*!
+import{g as er,R as Gn,c as Se,r as ae,a as tr}from"./index-48c6fc71.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
